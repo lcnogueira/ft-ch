9fb6d9c11233ff9126e531b6fae7aa79736814d3
Luiz Cláudio <luizcns18@gmail.com>
2021-07-06 08:13:57 -0300

    Handle static pages error

--- src/pages/boutique/[slug].tsx
+++ src/pages/boutique/[slug].tsx
@@ -12,19 +12,23 @@ export default function Boutique({ boutique }: BoutiquesTemplateProps) {
 }
 
 export async function getStaticPaths() {
-  const response = await fetch(process.env.API_ENDPOINT as string)
-  const boutiques: BoutiqueProp[] = await response.json()
-  const paths = boutiques.slice(1, 20).map(({ slug }) => ({
-    params: { slug }
-  }))
-
-  return { paths, fallback: 'blocking' }
+  try {
+    const response = await fetch(process.env.API_ENDPOINT as string)
+    const boutiques: BoutiqueProp[] = await response.json()
+    const paths = boutiques.slice(1, 20).map(({ slug }) => ({
+      params: { slug }
+    }))
+    return { paths, fallback: 'blocking' }
+  } catch (error) {
+    return { paths: [], fallback: 'blocking' }
+  }
 }
 
 export const getStaticProps: GetStaticProps = async ({ params }) => {
   const response = await fetch(process.env.API_ENDPOINT as string)
   const boutiques: BoutiqueProp[] = await response.json()
 
+  //we would hit another api route in this case (e.g.: /boutique/[slug]). Since we don'thave this public route, I just filtered the data.
   const boutique = boutiques.find((boutique) => boutique.slug === params?.slug)
 
   if (!boutique) return { notFound: true }
