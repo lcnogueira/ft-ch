668aa4492887ec1b5d91cf7382a72d5601b03751
Luiz Cláudio <luizcns18@gmail.com>
2021-07-08 09:00:18 -0300

    Improve env var config file

--- src/pages/boutique/[slug].tsx
+++ src/pages/boutique/[slug].tsx
@@ -16,7 +16,7 @@ export default function Boutique({ boutique }: BoutiquesTemplateProps) {
 
 export async function getStaticPaths() {
   try {
-    const response = await fetch(API_ENDPOINT as string)
+    const response = await fetch(API_ENDPOINT)
     const boutiques: Boutique[] = await response.json()
     const paths = boutiques.slice(0, NUMBER_OF_PAGES).map(({ slug }) => ({
       params: { slug }
@@ -28,7 +28,7 @@ export async function getStaticPaths() {
 }
 
 export const getStaticProps: GetStaticProps = async ({ params }) => {
-  const response = await fetch(API_ENDPOINT as string)
+  const response = await fetch(API_ENDPOINT)
   const boutiques: Boutique[] = await response.json()
 
   //we would hit another api route in this case (e.g.: /boutique/[slug]). Since we don'thave this public route, I just filtered the data.
